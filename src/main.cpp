86d2eda14ab390ea934c054dc0c601a1aa1d5653
Stephen M. Webb <stephen.webb@bregmasoft.com>
2009-03-08 12:47:05 -0400

    Futzed stdout and stderr for Windows.
    Signed-off-by: Stephen M. Webb <stephen.webb@bregmasoft.com>

--- src/main.cpp
+++ src/main.cpp
@@ -17,12 +17,51 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
+#include <fstream>
+#include <iostream>
 #include "nodice/app.h"
 #include <SDL/SDL_main.h>
 
 
+namespace
+{
+	/**
+	 * A class that futzes around wit cerr to force-redirect it to a log file.
+	 * This is mostly for use on windows or under a GUI where stderr is not
+	 * normally seen except by gooroos.
+	 */
+	class LogFutzer
+	{
+	public:
+		// Redirects stderr into a log file.
+		LogFutzer(std::ostream& stream)
+		: m_stream(stream)
+		, m_errbuf(m_stream.rdbuf())
+		, m_logfile("no-dice.log")
+		{
+			m_stream.rdbuf(m_logfile.rdbuf());
+		}
+
+		// Restores stderr.
+		~LogFutzer()
+		{
+			m_stream.rdbuf(m_errbuf);
+		}
+
+	private:
+		std::ostream&   m_stream;
+		std::streambuf* m_errbuf;
+		std::ofstream   m_logfile;
+	};
+} // anonymous namespace
+
+
 int main(int argc, char* argv[])
 {
+#if defined(_WIN32)
+	LogFutzer out(std::cout);
+	LogFutzer err(std::cerr);
+#endif
 	NoDice::App app(argc, argv);
 	return app.run();
 }
