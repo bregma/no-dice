73e32ee7751fbfe34350069e76e15da19bf03c56
Stephen M. Webb <stephen.webb@xandros.com>
2010-05-06 21:48:02 -0400

    Added a default nodice_config.h to windows build.
    This file is not autogenerated by MSVC so a handcrafted one needs to be supplied
    instead.
    
    Signed-off-by: Stephen M. Webb <stephen.webb@xandros.com>

--- nodice/main.cpp
+++ nodice/main.cpp
@@ -17,6 +17,8 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
+#include "nodice_config.h"
+
 #include <fstream>
 #include <iostream>
 #include "nodice/app.h"
@@ -69,6 +71,7 @@ int main(int argc, char* argv[])
 #endif
 	try
 	{
+		std::cerr << PACKAGE_STRING << "\n";
 		Config config(argc, argv);
 		return NoDice::App(config).run();
 
---
+++ windows/nodice_config.h
@@ -0,0 +1,65 @@
+/* nodice_config.h.  Generated from nodice_config.h.in by configure.  */
+/* nodice_config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Use EGL for the video context */
+/* #undef HAVE_EGL */
+
+/* Define to 1 if you have the <GLES/egl.h> header file. */
+/* #undef HAVE_GLES_EGL_H */
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+/* #undef HAVE_INTTYPES_H */
+
+/* Define to 1 if you have the <memory.h> header file. */
+/* #undef HAVE_MEMORY_H */
+
+/* Use OpenGL ES */
+/* #undef HAVE_OPENGL_ES */
+
+/* Define to 1 if you have the <stdint.h> header file. */
+/* #undef HAVE_STDINT_H */
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+/* #undef HAVE_STDLIB_H */
+
+/* Define to 1 if you have the <strings.h> header file. */
+/* #undef HAVE_STRINGS_H */
+
+/* Define to 1 if you have the <string.h> header file. */
+/* #undef HAVE_STRING_H */
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+/* #undef HAVE_SYS_STAT_H */
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+/* #undef HAVE_SYS_TYPES_H */
+
+/* Define to 1 if you have the <unistd.h> header file. */
+/* #undef HAVE_UNISTD_H */
+
+/* Name of package */
+#define PACKAGE "no-dice"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "stephen.webb@bregmasoft.com"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "no-dice"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "no-dice 1.0 for Microsoft Windows"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "no-dice"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "1.0"
+
+/* Define to 1 if you have the ANSI C header files. */
+/* #undef STDC_HEADERS */
+
+/* Version number of package */
+#define VERSION "1.0"
